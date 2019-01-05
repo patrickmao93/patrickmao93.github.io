148dfd17316e97b0d51527073ee8bac0df6278e5
Patrick <patrickmao93@gmail.com>
2019-01-05 18:13:56 +0800

    init

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Nav from "./Nav";
+
+const Header = props => {
+  return (
+    <div className="header">
+      <Nav />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <a className="nav__link" href="/">
+        Home
+      </a>
+      <a className="nav__link" href="/">
+        Portfolio
+      </a>
+      <a className="nav__link" href="/">
+        Blog
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;

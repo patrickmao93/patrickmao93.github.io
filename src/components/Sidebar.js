86d920de6e9efec40f36fc88fe274b4d780777f2
Patrick <patrickmao93@gmail.com>
2019-01-05 23:35:19 +0800

    added icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,18 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <i className="fas fa-cog" />
+      <a className="sidebar__icon" href="https://github.com/patrickmao93">
+        <i className="fab fa-github" />
+      </a>
+      <a className="sidebar__icon" href="/">
+        <i className="fab fa-linkedin-in" />
+      </a>
+      <a className="sidebar__icon" href="/">
+        <img src="../icons\zhihu.svg" alt="zhihu" />
+      </a>
+      <a className="sidebar__icon sidebar__setting" href="/">
+        <i className="fas fa-cog" />
+      </a>
     </div>
   );
 };

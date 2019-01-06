b212d3d7095f83412b52c7aa381aa63052b16397
Patrick <patrickmao93@gmail.com>
2019-01-06 21:57:05 +0800

    still playing with mockup code background, converted line class to react class component

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,37 +1,32 @@
 import React from "react";
 
-class Line {
-  constructor(color, length, index) {
-    this.color = color;
-    this.length = length;
-    this.index = index;
-  }
-
-  html() {
+class Line extends React.Component {
+  render() {
     return (
-      <span
-        key={this.index}
-        className={`line ${this.color} w-${this.length}`}
-      />
+      <span className={`line ${this.props.color} w-${this.props.length}`} />
     );
   }
 }
 
 class Code extends React.Component {
+  state = {
+    lines: []
+  };
+  componentDidMount() {}
+
   generateLines(amount) {
     const colors = ["blue", "purple", "green", "yellow", "white", "red"];
     const lines = [];
     for (let i = 0; i < amount; i++) {
       const color = colors[Math.floor(Math.random() * 6)];
-      const length = Math.ceil(Math.random() * 6);
-      const line = new Line(color, length, i);
-      lines.push(line.html());
+      const length = Math.ceil(Math.random() * 5);
+      lines.push(<Line key={i} color={color} length={length} />);
     }
     return lines;
   }
 
   render() {
-    return <div className="code">{this.generateLines(50)}</div>;
+    return <div className="code">{this.generateLines(100)}</div>;
   }
 }
 

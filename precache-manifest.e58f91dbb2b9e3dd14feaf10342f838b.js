f7671a6c7efd176088c45ee212dfb6a78c009a60
Patrick <patrickmao93@gmail.com>
2019-01-09 02:48:11 +0800

    Updates

---
+++ precache-manifest.e58f91dbb2b9e3dd14feaf10342f838b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "005bf475462b8be53fe087e6f1a52720",
+    "url": "/static/media/zhihu.005bf475.svg"
+  },
+  {
+    "revision": "0cdecdd63529b1e9c22612d16e328f21",
+    "url": "/static/media/segoeui_italic.0cdecdd6.ttf"
+  },
+  {
+    "revision": "3922dbc67cd4b0f95955a02787a17d0b",
+    "url": "/static/media/segoeui.3922dbc6.ttf"
+  },
+  {
+    "revision": "9b9631e5ca744324e42d2633467459ed",
+    "url": "/static/media/codewars.9b9631e5.svg"
+  },
+  {
+    "revision": "fb505e28b6d130f08fe8f070e0d6b1b8",
+    "url": "/static/media/CONSOLA.fb505e28.TTF"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c7e63c3217451e34c06",
+    "url": "/static/js/main.8c7e63c3.chunk.js"
+  },
+  {
+    "revision": "d41f28eaab9a28aa9a40",
+    "url": "/static/js/1.d41f28ea.chunk.js"
+  },
+  {
+    "revision": "8c7e63c3217451e34c06",
+    "url": "/static/css/main.d509eac8.chunk.css"
+  },
+  {
+    "revision": "33f283cc2adcd6033998992eb5954d5c",
+    "url": "/index.html"
+  }
+];

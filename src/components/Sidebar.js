dee96443f220691cadf95f93785fefbcf69a8f12
Patrick <patrickmao93@gmail.com>
2019-01-16 10:28:33 +0800

    added react-tooltip; added wechat QR code image; hovering sidebar icons now shows tooltip

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,57 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
+
 import zhihuIcon from "../icons/zhihu.svg";
 import codewarsIcon from "../icons/codewars.svg";
+
+import wechatQR from "../media/images/QR//wechat.jpg";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <a className="sidebar__icon" href="https://github.com/patrickmao93">
+      <a
+        className="sidebar__icon"
+        href="https://github.com/patrickmao93"
+        data-tip
+        data-for="github-tooltip"
+      >
+        <ReactTooltip id="github-tooltip" effect="solid">
+          <span>Github</span>
+        </ReactTooltip>
         <i className="fab fa-github" />
       </a>
-      <a className="sidebar__icon" href="/">
+      <a
+        className="sidebar__icon"
+        href="/"
+        data-tip
+        data-for="linkedin-tooltip"
+      >
+        <ReactTooltip id="linkedin-tooltip" effect="solid">
+          <span>LinkedIn</span>
+        </ReactTooltip>
         <i className="fab fa-linkedin-in" />
       </a>
-      <a className="sidebar__icon" href="/">
+      <a className="sidebar__icon" href="/" data-tip data-for="zhihu-tooltip">
+        <ReactTooltip id="zhihu-tooltip" effect="solid">
+          <span>Zhihu</span>
+        </ReactTooltip>
         <img src={zhihuIcon} alt="zhihu" />
       </a>
-      <a className="sidebar__icon" href="/">
+      <a
+        className="sidebar__icon"
+        href="/"
+        data-tip
+        data-for="codewars-tooltip"
+      >
+        <ReactTooltip id="codewars-tooltip" effect="solid">
+          <span>LinkedIn</span>
+        </ReactTooltip>
         <img src={codewarsIcon} alt="codewars" width="24" height="24" />
       </a>
-      <a className="sidebar__icon" href="/">
+      <a className="sidebar__icon" href="/" data-tip data-for="wechat-tooltip">
+        <ReactTooltip id="wechat-tooltip" effect="solid" place="right">
+          <img src={wechatQR} alt="wechat" />
+        </ReactTooltip>
         <i className="fab fa-weixin" />
       </a>
     </div>

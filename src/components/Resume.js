ed1db64c609dd078e37be92fffaa3fd93c49c239
Patrick <patrickmao93@gmail.com>
2019-01-18 17:05:56 +0800

    Resume desktop version finished

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,37 +1,43 @@
 import React from "react";
 import { Spring } from "react-spring";
 
+import resumeDoc from "../QizhengMao_Intermediate_Resume.docx";
+
 const Resume = () => {
   return (
     <div className="resume">
       <div className="resume__header">
         <h1>Qizheng Mao (Patrick)</h1>
         <h3>Full-Stack Web Developer</h3>
-        <section className="contact-section">
-          <span className="contact-section__info">
+        <section className="resume__header__section">
+          <span className="resume__header__section__info">
             <i className="fas fa-home" />
             600 Yushan Rd., #8 room 402, Shanghai
           </span>
-          <span className="contact-section__info">
+          <span className="resume__header__section__info">
             <i className="fas fa-envelope" />
             Patrickmao@live.com
           </span>
-          <span className="contact-section__info">
+          <span className="resume__header__section__info">
             <i className="fas fa-phone" />
             +86 135-0185-2846
           </span>
         </section>
-        <section className="intro-section">
+        <section className="resume__header__section">
           <p>
             Innovative, creative team player. 2 years of experience in web
             development and looking for more. Seek to secure a position of web
             development role.
           </p>
         </section>
-        <section className="tech-section">
-          <h2>Technologies I Use</h2>
-          <div className="tech-section__icons">
+        <section className="resume__header__section">
+          <h2>Technologies I'm Best At</h2>
+          <div className="resume__header__section__icons">
             <i className="fab fa-react" />
+            <i className="fab fa-js-square" />
+            <i className="fab fa-html5" />
+            <i className="fab fa-css3-alt" />
+            <i className="fab fa-node-js" />
           </div>
         </section>
       </div>
@@ -43,7 +49,7 @@ const Resume = () => {
           {props => (
             <div className="page" style={props}>
               <div className="page__download">
-                <a href="/" download>
+                <a href={resumeDoc} download>
                   <i className="fas fa-file-download" />
                   Download as Word Document
                 </a>
@@ -145,34 +151,50 @@ const Resume = () => {
                 <section>
                   <h3>Development Skills</h3>
                   <ul>
-                    <li>Proficient with version control system such as Git</li>
                     <li>
-                      Proficient with workflow tools - webpack, gulp, npm, grunt
+                      Proficient with version control system such as{" "}
+                      <strong>Git</strong>
+                    </li>
+                    <li>
+                      Proficient with workflow tools -{" "}
+                      <strong>webpack, gulp, npm, grunt</strong>
                     </li>
                     <li>
                       Proficient at adopting TDD/BDD principles; adept in
-                      testing tools such as Mocha
+                      testing tools such as <strong>Mocha</strong>
                     </li>
                     <li>
-                      Adept in front-end frameworks/libraries - React, Redux,
-                      Bootstrap, Materialize, and more
+                      Adept in front-end frameworks/libraries -{" "}
+                      <strong>React, Redux, Bootstrap, Materialize</strong>
                     </li>
-                    <li>Adept in back-end frameworks - Express, ASP.NET MVC</li>
                     <li>
-                      Experience in designing database schemas - MongoDB, MySQL
+                      Adept in back-end frameworks -{" "}
+                      <strong>Express, ASP.NET MVC</strong>
                     </li>
-                    <li>Experience in agile development and scrum method</li>
+                    <li>
+                      Experience in designing database schemas -{" "}
+                      <strong>MongoDB, MySQL</strong>
+                    </li>
+                    <li>
+                      Experience in <strong>agile</strong> development and{" "}
+                      <strong>scrum</strong> method
+                    </li>
                     <li>
                       Experience in doing code review and pair programming
                     </li>
-                    <li>Familiar with A/B testing</li>
+                    <li>
+                      Familiar with <strong>A/B testing</strong>
+                    </li>
                     <li>Keen understanding of UI/UX design</li>
                   </ul>
                 </section>
                 <section>
                   <h3>Design Skills</h3>
                   <ul>
-                    <li>Proficient with Adobe Creative Suite - XD, AI, PS</li>
+                    <li>
+                      Proficient with Adobe Creative Suite -{" "}
+                      <strong>XD, AI, PS</strong>
+                    </li>
                     <li>Proficient at wireframing and prototyping</li>
                     <li>Experience in conducting UX researches</li>
                   </ul>
